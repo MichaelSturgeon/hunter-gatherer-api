0cb1eab80ac1a2768bc3b0d99f9110ffc335e765
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-05 18:54:38 +0000

    Create profile list view & profile serializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles. Profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
