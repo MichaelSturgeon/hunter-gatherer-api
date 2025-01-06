43acd2581c03e977e7b697ca5d0c8493c22595c4
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-06 20:10:12 +0000

    Create review detail view

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from hg_api.permissions import IsOwnerOrReadOnly
 from .models import Review
 from .serializers import ReviewSerializer
 
@@ -10,3 +11,12 @@ class ReviewList(generics.ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+
+class ReviewDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve a review given an id,
+    and update a review given an authorized user.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
