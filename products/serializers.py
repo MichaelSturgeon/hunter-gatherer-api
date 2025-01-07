1c2970d71ba53df0afd113fe715b3f7ccf82f2bd
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-07 19:23:06 +0000

    Add filter feature to product list view & add reviews_count field in product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,10 +3,11 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    reviews_count = serializers.ReadOnlyField()
 
     class Meta:
         model = Product
         fields = [
             'id', 'name', 'description', 'price', 'updated_at', 'website',
-            'product_image', 'image_alt',
+            'product_image', 'image_alt', 'reviews_count',
         ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -7,13 +8,26 @@ class ProductList(generics.ListAPIView):
     """
     List all products.
     """
-    queryset = Product.objects.all()
+    queryset = Product.objects.annotate(
+        reviews_count=Count('review', distinct=True)
+    ).order_by('-updated_at')
     serializer_class = ProductSerializer
 
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'name',
+        'price',
+        'reviews_count',
+    ]
 
-class ProductDetail(generics.ListAPIView):
+
+class ProductDetail(generics.RetrieveAPIView):
     """
     Retrieve a product given a valid id.
     """
-    queryset = Product.objects.all()
+    queryset = Product.objects.annotate(
+        reviews_count=Count('review', distinct=True)
+    ).order_by('-updated_at')
     serializer_class = ProductSerializer
