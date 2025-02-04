ba4ee108f4272f7ca5fa245a56ae44bfbe47c496
Michael Sturgeon <1993msturgeon@gmail.com>
2025-02-04 09:11:56 +0000

    Add client origin variable

--- hg_api/settings.py
+++ hg_api/settings.py
@@ -100,6 +100,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")] 
+
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https:\/\/.*\.codeinstitute-ide\.net$",
